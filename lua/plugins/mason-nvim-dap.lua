e950fa56337310148b65a438be95138d23804096
Swapneel Amit Pathak <swapiitr@gmail.com>
2023-07-23 12:25:21 +0200

    feat(dap-python): add debug adapter protocol for python

---
+++ lua/plugins/mason-nvim-dap.lua
@@ -0,0 +1,9 @@
+return {
+  "jay-babu/mason-nvim-dap.nvim",
+  opts = {
+    ensure_installed = {
+      -- Update this to ensure that you have the debuggers for the langs you want
+      "debugpy",
+    },
+  },
+}

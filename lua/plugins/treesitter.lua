5be18b0478bb76d8f9da89bdeb1bb9f79a03d8be
Swapneel Amit Pathak <swapiitr@gmail.com>
2023-09-19 09:07:46 +0200

    fix(treesitter): stop highlights for latex

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -4,14 +4,10 @@ return {
     if type(opts.ensure_installed) == "table" then
       vim.list_extend(opts.ensure_installed, { "bibtex", "latex", "org" })
     end
-    if type(opts.highlight.disable) == "table" then
-      vim.list_extend(opts.highlight.disable, { "latex" })
-    else
-      opts.highlight.disable = { "latex" }
-    end
     opts.highlight = {
       enable = true,
       additional_vim_regex_highlighting = { "org" },
+      disable = { "latex" },
     }
   end,
 }

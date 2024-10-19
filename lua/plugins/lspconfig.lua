6ee6e0784b2eddcebb67ec171b904ce6ab1012df
Swapneel Amit Pathak <swapiitr@gmail.com>
2024-10-19 16:31:54 +0200

    refactor(lspconfig): stop ruff from grabbing hover action

--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -1,3 +1,7 @@
+local on_attach = function(client, bufnr)
+  client.server_capabilities.hoverProvider = false
+end
+
 return {
   "neovim/nvim-lspconfig",
   opts = {
@@ -21,5 +25,25 @@ return {
         },
       },
     },
+  },
+  setup = {
+    -- Specify * to use this function as a fallback for any server
+    -- ["*"] = function(server, opts) end,
+
+    ruff = function(_, opts)
+      -- Configure `ruff-lsp`.
+      -- See: https://github.com/neovim/nvim-lspconfig/blob/master/doc/server_configurations.md#ruff_lsp
+      -- For the default config, along with instructions on how to customize the settings
+      require("lspconfig").ruff.setup({
+        on_attach = on_attach,
+        init_options = {
+          settings = {
+            -- Any extra CLI arguments for `ruff` go here.
+            args = {},
+          },
+        },
+      })
+      return true
+    end,
   },
 }
